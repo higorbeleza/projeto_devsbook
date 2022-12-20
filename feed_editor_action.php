18b0ac167b8c30ff6cf39b434d1ef55520a3b3e5
Higor Beleza <higorbeleza49@gmail.com>
2022-12-20 16:07:49 -0300

    bug do home + bug no envio de post

--- feed_editor_action.php
+++ feed_editor_action.php
@@ -1,7 +1,7 @@
 <?php 
-require 'config.php';
-require 'models/Auth.php';
-require 'dao/PostDaoMysql.php';
+require_once 'config.php';
+require_once 'models/Auth.php';
+require_once 'dao/PostDaoMysql.php';
 
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
